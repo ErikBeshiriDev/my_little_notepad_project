20c1d6f85d71a42103932985750e261d959a4fdf
Erik Beshiri <148581637+ErikBeshiriDev@users.noreply.github.com>
2025-11-07 21:16:58 +0100

    ### Updates: - index.html (update 2) - standart.css (update 2)
    - Trash Option / - The ability to add notes and tasks to the trash and permanently delete them there.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+let notesTitles = [];
 let notes = [];
+
+let trashNotesTitles = [];
+let trashNotes = [];
+
 
 function renderNotes() {
     let contentRef = document.getElementById('content');
@@ -9,26 +14,56 @@ function renderNotes() {
     }
 }
 
+function renderTrashNotes() {
+    let trashContentRef = document.getElementById('trash_content')
+    trashContentRef.innerHTML = "";
+
+    for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
+        trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
+    }
+}
+
 function getNoteTemplate(indexNote) {
-    // return `<p>+ <title: ${notes[indexNote]}
-    return `<p>
-    <button onclick="deleteNote(${indexNote})">
+    return `<p>+ <title: ${notesTitles[indexNote]}
+    <button onclick="noteToTrash(${indexNote})"> 🗑️ Paper Bin
     ${notes[indexNote]} X</button>
     </p>`;  
+}
+
+function getTrashNoteTemplate(indexTrashNote) {
+    return `<p>+ title: ${trashNotesTitles[indexTrashNote]}
+    <button onclick="deleteTrashNote(${indexTrashNote})">
+    ${indexTrashNote} x</button>
+    </p>`
 }
 
 function addNote() {
     let noteInputRef = document.getElementById('new_note_or_task');
     let noteInput = noteInputRef.value;
+    if (noteInput.trim() !== '') {
+        notes.push(noteInput);
+        renderNotes();
+        noteInputRef.value = "";
+    } else {
+        alert("Please enter a note or task before adding.");
+    }
+}
 
-    notes.push(noteInput);
+function deleteTrashNote(indexTrashNote) {
+    trashNotesTitles.splice(indexTrashNote, 1);
+    trashNotes.splice(indexTrashNote, 1);
+    renderTrashNotes();
+}
 
+function noteToTrash(indexNote) {
+    let trashNote = notes.splice(indexNote, 1)[0];
+    trashNotes.push(trashNote);
     renderNotes();
-
-    noteInputRef.value = "";
+    renderTrashNotes();
 }
 
 function deleteNote(indexNote) {
     let trashNote = notes.splice(indexNote, 1);
     renderNotes();
+    renderTrashNotes();
 }
