149b919ebf2ec15393c733aeddaa15a8d33629e1
Erik Beshiri <148581637+ErikBeshiriDev@users.noreply.github.com>
2026-02-09 20:57:32 +0100

    Refactor allNotes structure; remove unused title properties and fix localStorage function calls

--- script.js
+++ script.js
@@ -2,29 +2,24 @@
 
 // Exkurs: Notizblock verbessern mit Objekten / Excursus: Improving the notepad with objects
 
-let allNotes {
-    'notesTitle': [],
+let allNotes = {
     'notes': [],
-    'archivNotesTitle': [],
     'archivNotes': [],
-    'trashNotes': [];
-    'trashNotesTitle': [];
+    'trashNotes': [],
 };
 
 function moveNote(indexNote, startKey, destinationKey) {
     let note = allNotes[startKey].splice(indexNote, 1);
-    allNotes[destinationKey + Titles].push(note[0]);
-
-    let notesTitle = allNotes[startKey].splice(indexNote, 1)
+    allNotes[destinationKey].push(note[0]);
 
     renderAllNotes();
-}
+};
 
 function renderAllNotes() {
     renderNotes();
     renderArchivNotes();
     renderTrashNotes();
-}
+};
 
 // DIALOGE
 
@@ -38,12 +33,12 @@ function saveToLocalStorage() {
     localStorage.setItem("content", JSON.stringify(allNotes.notes));
     localStorage.setItem("archivDialog", JSON.stringify(allNotes.archivNotes));
     localStorage.setItem("trashDialog", JSON.stringify(allNotes.trashNotes));
-}
+};
 
 function getFromLocalStorage() {
-    let notesLS = localStorange.getItem("content");
-    let archivLS = localStorange.getItem("archivDialog");
-    let trashLS = localStorange.getItem("trashDialog");
+    let notesLS = localStorage.getItem("content");
+    let archivLS = localStorage.getItem("archivDialog");
+    let trashLS = localStorage.getItem("trashDialog");
 
     if (notesLS) {
         allNotes.notes = JSON.parse(notesLS);
@@ -56,14 +51,14 @@ function getFromLocalStorage() {
     if (trashLS) {
         allNotes.trashNotes = JSON.parse(trashLS);
     }
-}
+};
 
 // INIT
 
 function init() {
     getFromLocalStorage();
     renderAllNotes();
-}
+};
 
 // RENDER
 
@@ -74,61 +69,61 @@ function renderNotes() {
     for (let indexNote = 0; indexNote < allNotes.notes.length; indexNote++) {
         contentRef.innerHTML += getNoteTemplate(indexNote);
     }
-}
+};
 
-function renderArchivNotes(indexArchivNotes) {
+function renderArchivNotes() {
     let archivContentRef = document.getElementById('archiv_content')
     archivContentRef.innerHTML = "";
 
     for (let indexArchivNote = 0; indexArchivNote < allNotes.archivNotes.length; indexArchivNote++) {
         archivContentRef.innerHTML += getArchivNoteTemplate(indexArchivNote);
-    }
-}
+    };
+};
 
-function renderTrashNotes(indexTrashNote) {
+function renderTrashNotes() {
     let trashContentRef = document.getElementById('trash_content')
     trashContentRef.innerHTML = "";
 
     for (let indexTrashNote = 0; indexTrashNote < allNotes.trashNotes.length; indexTrashNote++) {
         trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
     }
-}
+};
 
 // TEMPLATES
 
 function getNoteTemplate(indexNote) {
     return `
     <div class="content_item">
-        <h3>${notes[indexNote].title}</h3>
-        <p>${notes[indexNote].text}</p>
+        <h3>${allNotes.notes[indexNote].title}</h3>
+        <p>${allNotes.notes[indexNote].text}</p>
         <button onclick="moveNote(${indexNote},'notes', 'archivNotes')">🗂️</button>
         <button onclick="moveNote(${indexNote},'notes', 'trashNotes')">❌ -> 🗑️</button>
     </div>`;
-}
+};
 
 function getArchivNoteTemplate(indexArchivNote) {
     return `
     <div class="content_item">
         <div id="archiv_content_item_the_text">
-            <h3>${archivNotes[indexArchivNote].title}</h3>
-            <p>${archivNotes[indexArchivNote].text}</p>
+            <h3>${allNotes.archivNotes[indexArchivNote].title}</h3>
+            <p>${allNotes.archivNotes[indexArchivNote].text}</p>
         </div >
-        <button onclick="moveNote(${indexNote},'archivNotes', 'notes')">↩️</button>
-        <button onclick="moveNote(${indexNote},'archivNotes', 'trashNotes')">❌ -> 🗑️</button>
+        <button onclick="moveNote(${indexArchivNote},'archivNotes', 'notes')">↩️</button>
+        <button onclick="moveNote(${indexArchivNote},'archivNotes', 'trashNotes')">❌ -> 🗑️</button>
     </div > `;
-}
+};
 
 function getTrashNoteTemplate(indexTrashNote) {
     return `
     <div class="content_item">
         <div id="trash_content_item_the_text">
-            <p>${trashNotes[indexTrashNote].title}</p>
-            <p>${trashNotes[indexTrashNote].text}</p>
+            <p>${allNotes.trashNotes[indexTrashNote].title}</p>
+            <p>${allNotes.trashNotes[indexTrashNote].text}</p>
         </div>
-        <button onclick="moveNote(${indexNote},'trashNotes', 'notes')">↩️</button>
-        <button onclick="moveNote(${indexNote},'trashNotes', 'archivNotes')">🗑️</button>
+        <button onclick="moveNote(${indexTrashNote},'trashNotes', 'notes')">↩️</button>
+        <button onclick="moveNote(${indexTrashNote},'trashNotes', 'archivNotes')">🗑️</button>
     </div > `;
-}
+};
 
 // ADD NOTE
 
@@ -170,7 +165,7 @@ function addNote() {
     renderNotes();
     titleRef.value = ""
     textRef.value = ""
-}
+};
 
 // DIALOG CONTROL
 
@@ -180,7 +175,7 @@ function showDialog() {
         return;
     }
     dlg.showModal();
-}
+};
 
 function closeDialog() {
     if (!dlg) {
@@ -188,7 +183,7 @@ function closeDialog() {
         return;
     }
     dlg.close();
-}
+};
 
 function closeArchivDialog() {
     if (!archivDialog) {
@@ -196,7 +191,7 @@ function closeArchivDialog() {
         return;
     }
     archivDialog.close();
-}
+};
 
 function showArchivDialog() {
     if (!archivDialog) {
@@ -204,7 +199,7 @@ function showArchivDialog() {
         return;
     }
     archivDialog.showModal();
-}
+};
 
 function closeTrashDialog() {
     if (!trashDialog) {
@@ -212,7 +207,7 @@ function closeTrashDialog() {
         return;
     }
     trashDialog.close();
-}
+};
 
 function showTrashDialog() {
     if (!trashDialog) {
@@ -220,275 +215,4 @@ function showTrashDialog() {
         return;
     }
     trashDialog.showModal();
-}
-
-// --------------------------------------------------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------------------------------------------------
-
-// JS Old Version without Objects
-
-// STATE
-
-// let notes = [];
-// let archivNotes = [];
-// let trashNotes = [];
-
-// DIALOGE
-
-// const dlg = document.querySelector(".myDialog");
-// const archivDialog = document.querySelector(".myArchivDialog");
-// const trashDialog = document.querySelector(".myTrashDialog");
-
-// LOCAL STORAGE
-
-// function saveToLocalStorage() {
-//     localStorage.setItem("content", JSON.stringify(notes));
-//     localStorage.setItem("archivDialog", JSON.stringify(archivNotes));
-//     localStorage.setItem("trashDialog", JSON.stringify(trashNotes));
-// }
-
-// function getFromLocalStorage() {
-//     let notesLS = localStorange.getItem("content");
-//     let archivLS = localStorange.getItem("archivDialog");
-//     let trashLS = localStorange.getItem("trashDialog");
-
-//     if (notesLS) {
-//         notes = JSON.parse(notesLS);
-//     }
-
-//     if (archivLS) {
-//         archivNotes = JSON.parse(archivLS);
-//     }
-
-//     if (trashLS) {
-//         trashNotes = JSON.parse(trashLS);
-//     }
-// }
-
-// INIT
-
-// function init() {
-//     getFromLocalStorage();
-//     renderNotes();
-//     renderArchivNotes();
-//     renderTrashNotes();
-// }
-
-// RENDER
-
-// function renderNotes() {
-//     let contentRef = document.getElementById('content');
-//     contentRef.innerHTML = "";
-
-//     for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-//         contentRef.innerHTML += getNoteTemplate(indexNote);
-//     }
-// }
-
-// function renderArchivNotes(indexArchivNotes) {
-//     let archivContentRef = document.getElementById('archiv_content')
-//     archivContentRef.innerHTML = "";
-
-//     for (let indexArchivNote = 0; indexArchivNote < archivNotes.length; indexArchivNote++) {
-//         archivContentRef.innerHTML += getArchivNoteTemplate(indexArchivNote);
-//     }
-// }
-
-// function renderTrashNotes(indexTrashNote) {
-//     let trashContentRef = document.getElementById('trash_content')
-//     trashContentRef.innerHTML = "";
-
-//     for (let indexTrashNote = 0; indexTrashNote < trashNotes.length; indexTrashNote++) {
-//         trashContentRef.innerHTML += getTrashNoteTemplate(indexTrashNote);
-//     }
-// }
-
-// TEMPLATES
-
-// function getNoteTemplate(indexNote) {
-//     return `
-//     <div class="content_item">
-//         <h3>${notes[indexNote].title}</h3>
-//         <p>${notes[indexNote].text}</p>
-//         <button onclick="noteToArchiv(${indexNote})">🗂️</button>
-//         <button onclick="noteFromNotesToTrash(${indexNote})">❌ -> 🗑️</button>
-//     </div>`;
-// }
-
-// function getArchivNoteTemplate(indexArchivNote) {
-//     return `
-//     <div class="content_item">
-//         <div id="archiv_content_item_the_text">
-//             <h3>${archivNotes[indexArchivNote].title}</h3>
-//             <p>${archivNotes[indexArchivNote].text}</p>
-//         </div >
-//         <button onclick="pushNoteFromArchivBackToTheAnotherNotes(${indexArchivNote})">↩️</button>
-//         <button onclick="noteFromArchivToTrash(${indexArchivNote})">❌ -> 🗑️</button>
-//     </div > `;
-// }
-
-// function getTrashNoteTemplate(indexTrashNote) {
-//     return `
-//     <div class="content_item">
-//         <div id="trash_content_item_the_text">
-//             <p>${trashNotes[indexTrashNote].title}</p>
-//             <p>${trashNotes[indexTrashNote].text}</p>
-//         </div>
-//         <button onclick="pushNoteFromTrashBackToTheAnotherNotes(${indexTrashNote})">↩️</button>
-//         <button onclick="deleteTrashNote(${indexTrashNote})">🗑️</button>
-//     </div > `;
-// }
-
-// ADD NOTE
-
-// Ist die Funktion unten namens addNote zu lang bzw. Bis 14 Zeilen!?!?
-
-// function addNote() {
-
-//     let titleRef = document.getElementById('new_title_for_the_note_or_task');
-//     let textRef = document.getElementById('new_note_or_task');
-
-//     let title = titleRef.value.trim();
-//     let text = textRef.value.trim();
-
-//     if (text === "") {
-//         alert("Bitte Text eingeben");
-//         return;
-//     }
-
-//     if (title === "") {
-//         let wantsTitle = confirm("KeinTitel eingegeben. Willst du einen Titel schreiben?");
-//         if (!wantsTitle) {
-//             title = text.length > 10
-//                 ? text.slice(0, 10) + "..."
-//                 : text;
-//         } else {
-//             return;  
-//         }
-//     }
-
-//     notes.push({
-//         title: title,
-//         text: text
-//     });
-
-//     let inputTitelRef = document.getElementById('new_title_for_the_note_or_task');
-//     let textRef = document.getElementById('new_note_or_task');
-
-//     saveToLocalStorage();
-//     renderNotes();
-//     titleRef.value = ""
-//     textRef.value = ""
-// }
-
-// MOVE FUNCTIONS
-
-// function noteToArchiv(indexNote) {
-//     let toArchivNote = notes.splice(indexNote, 1)[0];
-//     archivNotes.push(toArchivNote);
-//     saveToLocalStorage();
-//     renderNotes();
-//     renderArchivNotes();
-// }
-
-// function pushNoteFromArchivBackToTheAnotherNotes(indexArchivNote) {
-//     let sendNoteBackToTheAnotherNotes = archivNotes.splice(indexArchivNote, 1)[0];
-//     notes.push(sendNoteBackToTheAnotherNotes);
-//     renderNotes();
-//     renderArchivNotes();
-//     saveToLocalStorage();
-// }
-
-// function pushNoteFromTrashBackToTheAnotherNotes(indexTrashNote) {
-//     let restoreNotetoTrash = trashNotes.splice(indexTrashNote, 1)[0];
-//     notes.push(restoreNotetoTrash);
-//     renderNotes();
-//     renderTrashNotes();
-//     saveToLocalStorage();
-// }
-
-// function noteFromArchivToTrash(indexArchivNote) {
-//     let trashNoteFromArchiv = archivNotes.splice(indexArchivNote, 1)[0];
-//     trashNotes.push(trashNoteFromArchiv);
-//     renderArchivNotes();
-//     renderTrashNotes();
-//     saveToLocalStorage();
-// }
-
-// function noteFromNotesToTrash(indexNote) {
-//     let trashNoteFromNotes = notes.splice(indexNote, 1)[0];
-//     trashNotes.push(trashNoteFromNotes);
-//     renderNotes();
-//     renderTrashNotes();
-//     saveToLocalStorage();
-// }
-
-// function deleteTrashNote(indexTrashNote) {
-//     trashNotes.splice(indexTrashNote, 1);
-//     renderTrashNotes();
-//     saveToLocalStorage();
-// }
-
-// function toggleOverlay() {
-//     let refOverlay = document.getElementById('myOverlayDialog');
-//     init();
-
-//     refOverlay.classList.toggle('dialog_hidden');
-// }
-
-// DIALOG CONTROL
-
-// function showDialog() {
-//     if (!dlg) {
-//         console.error("Dialog element not found!");
-//         return;
-//     }
-//     dlg.showModal();
-// }
-
-// function closeDialog() {
-//     if (!dlg) {
-//         console.error("Dialog element not found!");
-//         return;
-//     }
-//     dlg.close();
-// }
-
-// function closeArchivDialog() {
-//     if (!archivDialog) {
-//         console.error("Dialog element not found!");
-//         return;
-//     }
-//     archivDialog.close();
-// }
-
-// function showArchivDialog() {
-//     if (!archivDialog) {
-//         console.error("Dialog element not found!");
-//         return;
-//     }
-//     archivDialog.showModal();
-// }
-
-// function closeTrashDialog() {
-//     if (!trashDialog) {
-//         console.error("Dialog element not found!");
-//         return;
-//     }
-//     trashDialog.close();
-// }
-
-// function showTrashDialog() {
-//     if (!trashDialog) {
-//         console.error("Dialog element not found!");
-//         return;
-//     }
-//     trashDialog.showModal();
-// }
-
-
-
-
-
+};
